c27d5866931df946b39bd6ada013b1711203611e
Frank Shan <frank_shan@ziyutecc-ai.localdomain>
2025-11-26 08:53:12 -0800

    changes

--- test-cuda-availability.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-print(f"CUDA Available: {torch.cuda.is_available()}")
-print(f"PyTorch Version: {torch.__version__}")
-print(f"CUDA Version: {torch.version.cuda}")
-print(f"Supported Archs: {torch.cuda.get_arch_list()}")
-
-# if supported archs include sm_120, then CUDA is available for 5090
