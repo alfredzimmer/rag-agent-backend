d53851055101b08817bfc8bb688043a768155144
Andy Liu <andy.liu@ziyutec.com>
2025-11-26 09:31:18 -0800

    delete debug

--- src/rag/agent.py
+++ src/rag/agent.py
@@ -13,7 +13,6 @@ from modules.reranker import rerank
 #         for doc in retrieved_docs
 #     )
 #     return serialized, retrieved_docs
-# test git
 
 def hybrid_RAG_retrieve(query: str):
     """
